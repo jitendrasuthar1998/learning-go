4ed263900c6c2bbdb94d27a51995cadfb38ef4e9
Jitendra Suthar <jitendra.suthar@growcify.com>
2022-12-19 14:37:24 +0530

    learning go

---
+++ booking-app/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of ", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+	fmt.Println("how many ticket you want to book?")
+	fmt.Scan(&userTickets)
+
+	// fmt.Printf("User %v booked %v tickets.\n", firstName, userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confimation email at %v \n", firstName, lastName, userTickets, email)
+}
